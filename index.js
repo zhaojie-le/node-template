2e7c93fa19466666ae5736f9a1df02815809fe25
zhaojie <zhaojie.jane@bytedance.com>
2018-12-03 12:47:36 +0800

    feat: 增加路径配置

--- index.js
+++ index.js
@@ -12,20 +12,18 @@ const ora = require('ora');                          //提示下载
 var inquirer = require('inquirer');                  //提示文本
 const package = require('./package.json');           //获取版本信息
 const re = new RegExp("^[a-zA-Z]+$");                //检查文件名是否是英文，只支持英文
-const path = require('path')
-
+const basePath = `src/routes`
 program
   .version(package.version, '-v,--version')
   .command('init <name>')
   .action((name) => {
-    if (!re.test(name)) {
-      console.log(symbols.error, chalk.red(`${name}不合规!请输入英文名称！`));
-      return 
+    const path = name.split('/')
+    const endPath = `${basePath}/${name}`
+    const file = path[path.length -1]
+    if (fs.existsSync(endPath)) {
+      console.log(symbols.error, chalk.red(`${file}文件夹已存在`));
+      return
     }
-    if (fs.existsSync(name)) {
-      console.log(symbols.error, chalk.red(`${name}文件夹已存在`));
-    }
-    if (!fs.existsSync(name)) {
       inquirer
         .prompt([
           {
@@ -44,12 +42,12 @@ program
           const spinner = ora('loading......');
           spinner.start();
           const type = getType(answers)
-          downLoadFile(type, name)
+          downLoadFile(type, endPath)
           .then(
             files => {
               spinner.succeed();
               if (type === 'react-component') {
-                templateParse(files, name, type)
+                templateParse(files, endPath, type)
               }
               console.log(symbols.success, chalk.green('Created successfully!'));
             }
@@ -59,7 +57,7 @@ program
           )
 
         });
-    }
+    // }
   });
 
 program.parse(process.argv);
@@ -108,13 +106,15 @@ const templateParse = (files, name, type) => {
     }
   }
   if (type === 'react-component') {
+    const path = name.split('/')
+    const fil = path[path.length -1]
     files.forEach((item)=> {
       //获取文件列表
       if (!(item.includes('conf')||item.includes('index'))) {
         const index = item.indexOf('.')
         fs.rename(
           `${name}/${item}`,
-          `${name}/${name}${item.substring(index)}`,
+          `${name}/${fil}${item.substring(index)}`,
           err => {
             if (err) {
               console.log('err', err)
